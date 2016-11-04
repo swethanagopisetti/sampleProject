9530233a1d9d0276a2bf1e788b58cde9fbc94476
Swethana <swethanagopisetti@gmail.com>
2016-11-03 22:19:24 -0500

    Add radio buttons

--- surveyPage.ios.js
+++ surveyPage.ios.js
@@ -1,8 +1,18 @@
 'use strict';
 import React, { Component, PropTypes } from 'react';
 import { NavigatorIOS, Text, TouchableHighlight, View, AppRegistry, StyleSheet, Image } from 'react-native';
+import RadioForm, {RadioButton, RadioButtonInput, RadioButtonLabel} from 'react-native-simple-radio-button';
 
-class Register extends Component {    
+var radio_props = [
+    {label: 'Yes', value:0 },
+    {label: 'No', value:1 }
+];
+class Register extends Component {   
+    getInitialState (){
+        return {
+            value:0,
+        }
+    }
     render() {
         let pic = {
             uri: 'https://static.squarespace.com/static/50305c7684ae7fae2e65756a/5220048ee4b053b3578fc38a/52200493e4b053b3578fe989/1277125524713/1000w/KC%20skyline%20night%20HDR%202.jpg' 
@@ -11,7 +21,8 @@ class Register extends Component {
             <Image style = {styles.image} source = {pic}>
                 <View style= {styles.view} >
                     <Text style = {styles.heading} >Welcome to TALKville!</Text>
-                    <Text style = {styles.text} >👀 Do you like the app?</Text>
+                    <Text style = {styles.text} > Do you like the app?</Text>
+                    <RadioForm radio_props={radio_props} initial={0} onPress={(value) => {this.setState({value:value})}} />
                     <Text style = {styles.text} >❓ How often do you use the app?</Text>
                 </View>
             </Image>
